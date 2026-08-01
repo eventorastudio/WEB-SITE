c581509d0144d4dc2812b1ae4c559d45d2ccf88d
Luis Pablo García Moncada <luispablomoncada584@gmail.com>
2026-07-31 22:48:35 -0600

    Se añadio la funcionalidad de App Check para mejorar la seguridad de la aplicación y protegerla contra el uso no autorizado. Esto se logra mediante la integración de ReCaptchaV3, que verifica que las solicitudes provienen de usuarios legítimos y no de bots.

--- admin/firebase.js
+++ admin/firebase.js
@@ -4,6 +4,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-check.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCXY7EV89zW_4voYql7IYZsU_Cyh0HcY68",
@@ -16,6 +17,13 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializar App Check
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Lef0W8tAAAAADATSwjyK6zGEbj2887wbeaXuPgJ'),
+  isTokenAutoRefreshEnabled: true
+});
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
