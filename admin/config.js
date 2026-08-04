ee507629c27b20727236ca68d564e626ad0c1b9d
Luis Pablo García Moncada <luispablomoncada584@gmail.com>
2026-08-04 14:25:17 -0600

    Se modifico el archivo config.js para cambiar la ruta del logotipo de Eventora Studio. La nueva ruta es "./assets/images/LOGO2.jpg".

--- admin/config.js
+++ admin/config.js
@@ -4,8 +4,7 @@
 export const CONFIG = {
     APP_NAME: "Eventora Studio",
     VERSION: "1.0.0",
-    // Cambiar por la ruta real del logotipo de Eventora Studio
-    LOGO: "./assets/images/LOGO2.jpg", 
+    logo: "./assets/images/LOGO2.jpg", 
     FAVICON: "./assets/images/favicon.ico",
     LOGIN_REDIRECT: "dashboard.html",
     LOGOUT_REDIRECT: "index.html"
