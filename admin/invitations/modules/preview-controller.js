c0e5181db96a4f7bc1ecde00683f350a91e7abdf
Luis Pablo García Moncada <luispablomoncada584@gmail.com>
2026-08-21 20:24:02 -0600

    Boton

--- admin/invitations/modules/preview-controller.js
+++ admin/invitations/modules/preview-controller.js
@@ -22,7 +22,7 @@ export function initPreviewController({
     if (!state) return () => {};
 
     const targetOrigin = window.location.origin;
-    const previewUrl = new URL('./preview/frame.html?v=phase101-aloha-mobile-locations-20260822', document.baseURI).href;
+    const previewUrl = new URL('./preview/frame.html?v=phase102-aloha-mobile-music-20260822', document.baseURI).href;
     let previewWindow = null;
     let shellReady = false;
     let queuedRender = null;
